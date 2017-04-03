50d2e8beb34dc45a8ce6d9a72ac626757dd05eaf
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-04-03 10:51:20 +0200

    Refactor cherry_picker, add --abort/--continue, --status flags

--- cherry_picker/cherry_picker.py
+++ cherry_picker/cherry_picker.py
@@ -7,6 +7,220 @@ import subprocess
 import webbrowser
 
 
+class CherryPicker:
+
+    def __init__(self, pr_remote, commit_sha1, branches,
+                 *, dry_run=False):
+        self.pr_remote = pr_remote
+        self.commit_sha1 = commit_sha1
+        self.branches = branches
+        self.dry_run = dry_run
+
+    @property
+    def upstream(self):
+        """Get the remote name to use for upstream branches
+        Uses "upstream" if it exists, "origin" otherwise
+        """
+        cmd = "git remote get-url upstream"
+        try:
+            subprocess.check_output(cmd.split(), stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError:
+            return "origin"
+        return "upstream"
+
+    @property
+    def sorted_branches(self):
+        return sorted(
+            self.branches,
+            reverse=True,
+            key=lambda v: tuple(map(int, v.split('.'))))
+
+    @property
+    def username(self):
+        cmd = f"git config --get remote.{self.pr_remote}.url"
+        raw_result = subprocess.check_output(cmd.split(),
+                                             stderr=subprocess.STDOUT)
+        result = raw_result.decode('utf-8')
+        username_end = result.index('/cpython.git')
+        if result.startswith("https"):
+            username = result[len("https://github.com/"):username_end]
+        else:
+            username = result[len("git@github.com:"):username_end]
+        return username
+
+    def get_cherry_pick_branch(self, maint_branch):
+        return f"backport-{self.commit_sha1[:7]}-{maint_branch}"
+
+    def get_pr_url(self, base_branch, head_branch):
+        return f"https://github.com/python/cpython/compare/{base_branch}...{self.username}:{head_branch}?expand=1"
+
+    def fetch_upstream(self):
+        """ git fetch <upstream> """
+        # self.run_cmd(f"git fetch {self.upstream}")
+        pass
+
+    def run_cmd(self, cmd, shell=False):
+        if self.dry_run:
+            click.echo(f"  dry-run: {cmd}")
+            return
+        if not shell:
+            output = subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
+            click.echo(output.decode('utf-8'))
+        else:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+
+    def checkout_branch(self, branch_name):
+        """ git checkout -b <branch_name> """
+        cmd = f"git checkout -b {self.get_cherry_pick_branch(branch_name)} {self.upstream}/{branch_name}"
+        try:
+            self.run_cmd(cmd)
+        except subprocess.CalledProcessError as err:
+            click.echo("error checking out branch")
+            click.echo(err)
+
+    def get_commit_message(self, commit_sha):
+        """
+        Return the commit message for the current commit hash,
+        replace #<PRID> with GH-<PRID>
+        """
+        cmd = f"git show -s --format=%B {commit_sha}"
+        output = subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
+        updated_commit_message = output.strip().decode('utf-8').replace('#', 'GH-')
+        return updated_commit_message
+
+    def checkout_master(self):
+        """ git checkout master """
+        cmd = "git checkout master"
+        self.run_cmd(cmd)
+
+    def status(self):
+        """
+        git status
+        :return:
+        """
+        cmd = "git status"
+        self.run_cmd(cmd)
+
+    def cherry_pick(self):
+        """ git cherry-pick -x <commit_sha1> """
+        cmd = f"git cherry-pick -x {self.commit_sha1}"
+        self.run_cmd(cmd)
+
+    def get_exit_message(self, branch):
+        return \
+f"""
+Failed to cherry-pick {self.commit_sha1} into {branch} \u2639
+... Stopping here.
+
+To continue and resolve the conflict:
+    $ python -m cherry_picker --status  # to find out which files need attention
+    $ cd cpython
+    # Fix the conflict
+    $ cd ..
+    $ python -m cherry_picker --status  # should now say 'all conflict fixed'
+    $ python -m cherry_picker --continue
+
+To abort the cherry-pick and cleanup:
+    $ python -m cherry_picker --abort
+"""
+
+    def amend_commit_message(self, cherry_pick_branch):
+        """ prefix the commit message with (X.Y) """
+        base_branch = get_base_branch(cherry_pick_branch)
+
+        cmd = f"git commit --amend -m '[{base_branch}]: {self.get_commit_message(self.commit_sha1)}{os.linesep}(cherry picked from commit {self.commit_sha1})'"
+        self.run_cmd(cmd, shell=True)
+
+    def push_to_remote(self, base_branch, head_branch):
+        """ git push <origin> <branchname> """
+
+        cmd = f"git push {self.pr_remote} {head_branch}"
+        try:
+            self.run_cmd(cmd)
+        except:
+            click.echo(f"Failed to push to {self.pr_remote} \u2639")
+        else:
+            self.open_pr(self.get_pr_url(base_branch, head_branch))
+
+    def open_pr(self, url):
+        """
+        open url in the web browser
+        """
+        if self.dry_run:
+            click.echo(f"  dry-run: Create new PR: {url}")
+        else:
+            webbrowser.open_new_tab(url)
+
+    def delete_branch(self, branch):
+        cmd = f"git branch -D {branch}"
+        self.run_cmd(cmd)
+
+    def cleanup_branch(self, branch):
+        self.checkout_master()
+        try:
+            self.delete_branch(branch)
+        except subprocess.CalledProcessError:
+            click.echo(f"branch {branch} NOT deleted.")
+        else:
+            click.echo(f"branch {branch} has been deleted.")
+
+    def backport(self):
+        if not self.branches:
+            raise ValueError("At least one branch must be specified.")
+        self.fetch_upstream()
+
+        for maint_branch in self.sorted_branches:
+            click.echo(f"Now backporting '{self.commit_sha1}' into '{maint_branch}'")
+
+            cherry_pick_branch = self.get_cherry_pick_branch(maint_branch)
+            self.checkout_branch(maint_branch)
+            try:
+                self.cherry_pick()
+                self.amend_commit_message(cherry_pick_branch)
+            except subprocess.CalledProcessError:
+                click.echo(self.get_exit_message(maint_branch))
+            else:
+                self.push_to_remote(maint_branch, cherry_pick_branch)
+                self.cleanup_branch(cherry_pick_branch)
+
+
+    def abort_cherry_pick(self):
+        """
+        run `git cherry-pick --abort` and then clean up the branch
+        """
+        cmd = "git cherry-pick --abort"
+        try:
+            self.run_cmd(cmd) #, stdout=subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            self.cleanup_branch(get_current_branch())
+
+    def continue_cherry_pick(self):
+        """
+        git push origin <current_branch>
+        open the PR
+        clean up branch
+        """
+        cherry_pick_branch = get_current_branch()
+
+        if cherry_pick_branch != 'master':
+
+            # amend the commit message, prefix with [X.Y]
+            base = get_base_branch(cherry_pick_branch)
+            short_sha = cherry_pick_branch[cherry_pick_branch.index('-')+1:cherry_pick_branch.index(base)-1]
+            full_sha = get_full_sha_from_short(short_sha)
+            commit_message = self.get_commit_message(short_sha)
+
+            cmd = f"git commit -am '[{base}]: {commit_message}. \n(cherry picked from commit {full_sha})' --allow-empty"
+            self.run_cmd(cmd, shell=True)
+
+            self.push_to_remote(base, cherry_pick_branch)
+
+            self.cleanup_branch(cherry_pick_branch)
+        else:
+            click.echo(u"Refuse to push to master \U0001F61B")
+
 @click.command()
 @click.option('--dry-run', is_flag=True,
               help="Prints out the commands, but not executed.")
@@ -16,183 +230,45 @@ import webbrowser
               help="Abort current cherry-pick and clean up branch")
 @click.option('--continue', 'abort', flag_value=False, default=None,
               help="Continue cherry-pick, push, and clean up branch")
+@click.option('--status', 'status', flag_value = True, default=None,
+              help="Get the status of cherry-pick")
 @click.argument('commit_sha1', 'The commit sha1 to be cherry-picked', nargs=1,
                 default = "")
 @click.argument('branches', 'The branches to backport to', nargs=-1)
-def cherry_pick(dry_run, pr_remote, abort, commit_sha1, branches):
-    click.echo("\U0001F40D \U0001F352 \u26CF ")
-    if not os.path.exists('./pyconfig.h.in'):
-        os.chdir('./cpython/')
+def cherry_pick_cli(dry_run, pr_remote, abort, status, commit_sha1, branches):
 
-    upstream = get_git_fetch_remote()
-    username = get_forked_repo_name(pr_remote)
+    click.echo("\U0001F40D \U0001F352 \u26CF")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    pyconfig_path = os.path.join(current_dir, '/pyconfig.h.in')
+
+    if not os.path.exists(pyconfig_path):
+        os.chdir(os.path.join(current_dir, 'cpython'))
 
     if dry_run:
        click.echo("Dry run requested, listing expected command sequence")
 
+    cherry_picker = CherryPicker(pr_remote, commit_sha1, branches, dry_run=dry_run)
+
     if abort is not None:
         if abort:
-            abort_cherry_pick(dry_run=dry_run)
+            cherry_picker.abort_cherry_pick()
         else:
-            continue_cherry_pick(username, pr_remote, dry_run=dry_run)
+            cherry_picker.continue_cherry_pick()
 
+    elif status:
+        cherry_picker.status()
     else:
-        backport_branches(commit_sha1, branches, username, upstream, pr_remote,
-                          dry_run=dry_run)
-
-def backport_branches(commit_sha1, branches, username, upstream, pr_remote,
-                      *, dry_run=False):
-    if commit_sha1 == "":
-        raise ValueError("Missing the commit_sha1 argument.")
-    if not branches:
-        raise ValueError("At least one branch is required.")
-    else:
-        run_cmd(f"git fetch {upstream}", dry_run=dry_run)
-
-        for branch in get_sorted_branch(branches):
-            click.echo(f"Now backporting '{commit_sha1}' into '{branch}'")
-
-            # git checkout -b backport-61e2bc7-3.5 upstream/3.5
-            cherry_pick_branch = f"backport-{commit_sha1[:7]}-{branch}"
-            pr_url = get_pr_url(username, branch, cherry_pick_branch)
-            cmd = f"git checkout -b {cherry_pick_branch} {upstream}/{branch}"
-            run_cmd(cmd, dry_run=dry_run)
-
-            cmd = f"git cherry-pick -x {commit_sha1}"
-            if run_cmd(cmd, dry_run=dry_run):
-                push_to_remote(pr_url, pr_remote, cherry_pick_branch, dry_run=dry_run)
-                cleanup_branch(cherry_pick_branch, dry_run=dry_run)
-            else:
-                click.echo(f"Failed to cherry-pick {commit_sha1} into {branch} \u2639")
-                click.echo(" ... Stopping here. ")
-
-                click.echo("")
-                click.echo("To continue and resolve the conflict: ")
-                click.echo("    $ cd cpython")
-                click.echo("    $ git status # to find out which files need attention")
-                click.echo("    # Fix the conflict")
-                click.echo("    $ git status # should now say `all conflicts fixed`")
-                click.echo("    $ cd ..")
-                click.echo("    $ python -m cherry_picker --continue")
-
-                click.echo("")
-                click.echo("To abort the cherry-pick and cleanup: ")
-                click.echo("    $ python -m cherry_picker --abort")
-
-
-def abort_cherry_pick(*, dry_run=False):
-    """
-    run `git cherry-pick --abort` and then clean up the branch
-    """
-    if run_cmd("git cherry-pick --abort", dry_run=dry_run):
-        cleanup_branch(get_current_branch(), dry_run=dry_run)
-
-
-def continue_cherry_pick(username, pr_remote, *, dry_run=False):
-    """
-    git push origin <current_branch>
-    open the PR
-    clean up branch
-
-    """
-    cherry_pick_branch = get_current_branch()
-    if cherry_pick_branch != 'master':
-
-        # this has the same effect as `git cherry-pick --continue`
-        cmd = f"git commit -am 'Resolved.' --allow-empty"
-        run_cmd(cmd, dry_run=dry_run)
-
-        base_branch = get_base_branch(cherry_pick_branch)
-        pr_url = get_pr_url(username, base_branch, cherry_pick_branch)
-        push_to_remote(pr_url, pr_remote, cherry_pick_branch, dry_run=dry_run)
-
-        cleanup_branch(cherry_pick_branch, dry_run=dry_run)
-    else:
-        click.echo(u"Refuse to push to master \U0001F61B")
+        cherry_picker.backport()
 
 
 def get_base_branch(cherry_pick_branch):
     """
     return '2.7' from 'backport-sha-2.7'
     """
-    return cherry_pick_branch[cherry_pick_branch.rfind('-')+1:]
-
-
-def cleanup_branch(cherry_pick_branch, *, dry_run=False):
-    """
-    git checkout master
-    git branch -D <branch to delete>
-    """
-    cmd = "git checkout master"
-    run_cmd(cmd, dry_run=dry_run)
-
-    cmd = f"git branch -D {cherry_pick_branch}"
-    if run_cmd(cmd, dry_run=dry_run):
-        if not dry_run:
-            click.echo(f"branch {cherry_pick_branch} has been deleted.")
+    if '-' in cherry_pick_branch:
+        return cherry_pick_branch[cherry_pick_branch.rfind('-')+1:]
     else:
-        click.echo(f"branch {cherry_pick_branch} NOT deleted.")
-
-def push_to_remote(pr_url, pr_remote, cherry_pick_branch, *, dry_run=False):
-    cmd = f"git push {pr_remote} {cherry_pick_branch}"
-    if not run_cmd(cmd, dry_run=dry_run):
-        click.echo(f"Failed to push to {pr_remote} \u2639")
-    else:
-        open_pr(pr_url, dry_run=dry_run)
-
-def get_git_fetch_remote():
-    """Get the remote name to use for upstream branches
-    Uses "upstream" if it exists, "origin" otherwise
-    """
-    cmd = "git remote get-url upstream"
-    try:
-        subprocess.check_output(cmd.split(), stderr=subprocess.DEVNULL)
-    except subprocess.CalledProcessError:
-        return "origin"
-    return "upstream"
-
-
-def get_forked_repo_name(pr_remote):
-    """
-    Return 'myusername' out of https://github.com/myusername/cpython
-    """
-    cmd = f"git config --get remote.{pr_remote}.url"
-    raw_result = subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
-    result = raw_result.decode('utf-8')
-    username_end = result.index('/cpython.git')
-    if result.startswith("https"):
-        username = result[len("https://github.com/"):username_end]
-    else:
-        username = result[len("git@github.com:"):username_end]
-    return username
-
-
-def run_cmd(cmd, *, dry_run=False):
-    if dry_run:
-        click.echo(f"  dry-run: {cmd}")
-        return True
-    try:
-        subprocess.check_output(cmd.split())
-    except subprocess.CalledProcessError:
-        return False
-    return True
-
-
-def get_pr_url(forked_repo, base_branch, cherry_pick_branch):
-    """
-    construct the url for the pull request
-    """
-    return f"https://github.com/python/cpython/compare/{base_branch}...{forked_repo}:{cherry_pick_branch}?expand=1"
-
-
-def open_pr(url, *, dry_run=False):
-    """
-    open url in the web browser
-    """
-    if dry_run:
-        click.echo(f"  dry-run: Create new PR: {url}")
-        return
-    webbrowser.open_new_tab(url)
+        return cherry_pick_branch
 
 
 def get_current_branch():
@@ -200,16 +276,17 @@ def get_current_branch():
     Return the current branch
     """
     cmd = "git symbolic-ref HEAD | sed 's!refs\/heads\/!!'"
-    output = subprocess.check_output(cmd, shell=True)
-    return output.strip().decode()
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    return output.strip().decode('utf-8')
 
 
-def get_sorted_branch(branches):
-    return sorted(
-        branches,
-        reverse=True,
-        key=lambda v: tuple(map(int, v.split('.'))))
+def get_full_sha_from_short(short_sha):
+    cmd = f"git show --format=raw {short_sha}"
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    full_sha = output.strip().decode('utf-8').split('\n')[0].split()[1]
+    return full_sha
+
 
 
 if __name__ == '__main__':
-    cherry_pick()
+    cherry_pick_cli()
--- cherry_picker/test.py
+++ cherry_picker/test.py
@@ -1,7 +1,19 @@
-from . import cherry_picker
+from unittest import mock
 
+from .cherry_picker import get_base_branch, get_current_branch
 
-def test_sorted_branch():
-    branches = ["3.1", "2.7", "3.10", "3.6"]
-    result = cherry_picker.get_sorted_branch(branches)
-    assert result == ["3.10", "3.6", "3.1", "2.7"]
+def test_get_base_branch():
+    cherry_pick_branch = 'backport-afc23f4-2.7'
+    result = get_base_branch(cherry_pick_branch)
+    assert result == '2.7'
+
+def test_get_base_branch_without_dash():
+    cherry_pick_branch ='master'
+    result = get_base_branch(cherry_pick_branch)
+    assert result == 'master'
+
+@mock.patch('subprocess.check_output')
+def test_get_current_branch(subprocess_check_output):
+    subprocess_check_output.return_value = b'master'
+    assert get_current_branch() == 'master'
+
