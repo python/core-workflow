61ec2f4892fa9967845c9d8e8e74338a7bd5817e
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-06-17 22:32:41 -0700

    Move files to cherry_picker directory

--- cherry_picker/setup.py
+++ cherry_picker/setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
 
-with open('cherry_picker/requirements.txt') as f:
+with open('readme.rst') as f:
+    long_description = f.read()
+
+
+with open('requirements.txt') as f:
     requirements = [l.strip() for l in f.read().split('\n')]
 
-description = "Tools for working with CPython's new core development workflow."
 
 setup(
-    name='core-workflow',
-    description=description,
-    long_description=description,
+    name='cherry_picker',
+    description='Backport CPython changes from master to maintenance branches',
+    long_description=long_description,
     license='Apache',
     url='https://github.com/python/core-workflow',
-    author='Python Core Developers',
-    author_email='core-workflow@mail.python.org',
+    author='Mariatta Wijaya',
+    author_email='mariatta.wijaya@gmail.com',
     maintainer='Python Core Developers',
     maintainer_email='core-workflow@mail.python.org',
-    version='0.0.3',
+    version='0.0.2.post2',
     packages=[
         'cherry_picker',
     ],
     entry_points={
         'console_scripts': [
-            'cherry_picker = cherry_picker.cherry_picker:cherry_pick_cli',
+            'cherry_picker = cherry_picker:cherry_pick_cli',
         ],
     },
-    scripts=[
-        'blurb',
-    ],
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
