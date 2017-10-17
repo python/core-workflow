1885210896d1c566450216f99c4303d4b2409fc3
Mario Corchero <mcorcherojim@bloomberg.net>
2017-10-17 19:59:20 +0100

    Improve error report on subprocess exceptions
    At the moment when a subprocess command fails the error output is not
    meaningful at all. By logging the command run and the output that it
    produced the user can get a further insight into the root of the issue

--- blurb/blurb.py
+++ blurb/blurb.py
@@ -1596,6 +1596,10 @@ def main():
         print()
         print("usage: ", end="")
         help(subcommand)
+    except subprocess.SubprocessError as e:
+        output = e.stdout.decode() + e.stderr.decode()
+        print("Failed running {}, output: \n{}".format(e.cmd, output))
+        raise
 
 
 if __name__ == '__main__':
