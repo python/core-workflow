ff140ae9517c10abe849a998f2f396bc54b2fd8d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2018-05-14 16:20:15 -0400

    blurb: small usability improvements (#161)
    * blurb: small usability improvements
    
    * only suggest 'blurb help' for invalid commands; filter --help from help output
    
    * support other commands starting with "-" (none currently exist)

--- blurb/blurb.py
+++ blurb/blurb.py
@@ -744,7 +744,7 @@ def subcommand(fn):
 def get_subcommand(subcommand):
     fn = subcommands.get(subcommand)
     if not fn:
-        error("Unknown subcommand: {}".format(subcommand))
+        error("Unknown subcommand: {}\nRun 'blurb help' for help.".format(subcommand))
     return fn
 
 
@@ -771,6 +771,8 @@ If subcommand is not specified, prints one-line summaries for every command.
         summaries = []
         longest_name_len = -1
         for name, fn in subcommands.items():
+            if name.startswith('-'):
+                continue
             longest_name_len = max(longest_name_len, len(name))
             if not fn.__doc__:
                 error("help is broken, no docstring for " + fn.__name__)
@@ -820,6 +822,9 @@ If subcommand is not specified, prints one-line summaries for every command.
     print()
     print(doc)
     sys.exit(0)
+
+# Make "blurb --help" work.
+subcommands["--help"] = help
 
 
 @subcommand
