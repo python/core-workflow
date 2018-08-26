6bb308138f2ec4038febf84a739db942ed7a694b
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-08-26 22:08:17 +0200

    Guard init module with exception fallback

--- cherry_picker/cherry_picker/__init__.py
+++ cherry_picker/cherry_picker/__init__.py
@@ -1,25 +1,35 @@
 """Backport CPython changes from master to maintenance branches."""
 from pathlib import Path
 
-from setuptools_scm import get_version
-from setuptools_scm.git import parse
+try:
+    from setuptools_scm import get_version
+    from setuptools_scm.git import parse
 
 
-def parse_starting_with(prefix, *args, **kwargs):
-    """Extract tag with a given prefix."""
-    descr_cmd = (
-        'git describe --dirty --tags --long '
-        '--match {tag_prefix}-*'.format(tag_prefix=prefix)
+    def parse_starting_with(prefix, *args, **kwargs):
+        """Extract tag with a given prefix."""
+        descr_cmd = (
+            'git describe --dirty --tags --long '
+            '--match {tag_prefix}-*'.format(tag_prefix=prefix)
+        )
+        return parse(*args, describe_command=descr_cmd, **kwargs)
+
+    def get_parser_with_prefix(prefix):
+        """Return a function with git describe command substituted."""
+        # Couldn't do this via functools.partial, because of internal
+        # check for an argument being a function.
+        return lambda *a, **k: parse_starting_with(prefix, *a, **k) 
+
+    __version__ = get_version(
+        Path(__file__) / '..' / '..' / '..',
+        parse=get_parser_with_prefix('cherry-picker')
     )
-    return parse(*args, describe_command=descr_cmd, **kwargs)
-
-def get_parser_with_prefix(prefix):
-    """Return a function with git describe command substituted."""
-    # Couldn't do this via functools.partial, because of internal
-    # check for an argument being a function.
-    return lambda *a, **k: parse_starting_with(prefix, *a, **k) 
-
-__version__ = get_version(
-    Path(__file__) / '..' / '..' / '..',
-    parse=get_parser_with_prefix('cherry-picker')
-)
+except ImportError:
+    try:
+        import pkg_resources
+    except ImportError:
+        __version__ = 'unknown'
+    else:
+        __version__ = (
+            pkg_resources.get_distribution('cherry-picker').version
+        )
