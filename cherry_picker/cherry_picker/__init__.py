f1687c2bcaeed5321892080a4fab6d402fd3be89
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-08-26 20:24:36 +0200

    Move version data to scm (Git tags)

--- cherry_picker/cherry_picker/__init__.py
+++ cherry_picker/cherry_picker/__init__.py
@@ -1,2 +1,25 @@
 """Backport CPython changes from master to maintenance branches."""
-__version__ = '1.2.2.dev1'
+from pathlib import Path
+
+from setuptools_scm import get_version
+from setuptools_scm.git import parse
+
+
+def parse_starting_with(prefix, *args, **kwargs):
+    """Extract tag with a given prefix."""
+    descr_cmd = (
+        'git describe --dirty --tags --long '
+        '--match {tag_prefix}-*'.format(tag_prefix=prefix)
+    )
+    return parse(*args, describe_command=descr_cmd, **kwargs)
+
+def get_parser_with_prefix(prefix):
+    """Return a function with git describe command substituted."""
+    # Couldn't do this via functools.partial, because of internal
+    # check for an argument being a function.
+    return lambda *a, **k: parse_starting_with(prefix, *a, **k) 
+
+__version__ = get_version(
+    Path(__file__) / '..' / '..' / '..',
+    parse=get_parser_with_prefix('cherry-picker')
+)
