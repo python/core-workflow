a2226d04661e7ce680fe30f2a405ad80b4ef44fd
Mariatta <mariatta@python.org>
2019-06-04 20:43:59 -0700

    Remove the cherry_picker directory

--- cherry_picker/cherry_picker/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Backport CPython changes from master to maintenance branches."""
-__version__ = '1.3.3.dev1'
--- cherry_picker/cherry_picker/__main__.py
+++
@@ -1,4 +0,0 @@
-from .cherry_picker import cherry_pick_cli
-
-if __name__ == '__main__':
-    cherry_pick_cli()
