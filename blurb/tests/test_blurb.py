8e53d4a99f0c3d1d11066809f4de653e65584f62
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-07-23 20:22:22 +0200

    Add glob_blurbs test for sorting by filename

--- blurb/tests/test_blurb.py
+++ blurb/tests/test_blurb.py
@@ -71,6 +71,41 @@ def test_glob_blurbs_next(fs):
     assert set(filenames) == set(fake_news_entries)
 
 
+def test_glob_blurbs_sort_order(fs):
+    """
+    It shouldn't make a difference to sorting whether
+    section names have spaces or underscores.
+    """
+    # Arrange
+    fake_news_entries = (
+        "Misc/NEWS.d/next/Core and Builtins/2023-07-23-12-01-00.gh-issue-33331.Pf_BI1.rst",
+        "Misc/NEWS.d/next/Core_and_Builtins/2023-07-23-12-02-00.gh-issue-33332.Pf_BI2.rst",
+        "Misc/NEWS.d/next/Core and Builtins/2023-07-23-12-03-00.gh-issue-33333.Pf_BI3.rst",
+        "Misc/NEWS.d/next/Core_and_Builtins/2023-07-23-12-04-00.gh-issue-33334.Pf_BI4.rst",
+    )
+    # As fake_news_entries, but reverse sorted by *filename* only
+    expected = [
+        "Misc/NEWS.d/next/Core_and_Builtins/2023-07-23-12-04-00.gh-issue-33334.Pf_BI4.rst",
+        "Misc/NEWS.d/next/Core and Builtins/2023-07-23-12-03-00.gh-issue-33333.Pf_BI3.rst",
+        "Misc/NEWS.d/next/Core_and_Builtins/2023-07-23-12-02-00.gh-issue-33332.Pf_BI2.rst",
+        "Misc/NEWS.d/next/Core and Builtins/2023-07-23-12-01-00.gh-issue-33331.Pf_BI1.rst",
+    ]
+    fake_readmes = (
+        "Misc/NEWS.d/next/Library/README.rst",
+        "Misc/NEWS.d/next/Core and Builtins/README.rst",
+        "Misc/NEWS.d/next/Tools-Demos/README.rst",
+        "Misc/NEWS.d/next/C API/README.rst",
+    )
+    for fn in fake_news_entries + fake_readmes:
+        fs.create_file(fn)
+
+    # Act
+    filenames = blurb.glob_blurbs("next")
+
+    # Assert
+    assert filenames == expected
+
+
 @pytest.mark.parametrize(
     "news_entry, expected_section",
     (
